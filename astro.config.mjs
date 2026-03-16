c30e2e93a2bc13f8d0b1ae893ea8a7248e398c42
Federico Elizondo <federicoelizondom@gmail.com>
2026-03-16 13:42:10 -0600

    fix: fix astro.config.mjs and tsconfig.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
+import node from '@astrojs/node';
 import auth from 'auth-astro';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: node({ mode: 'standalone' }),
+  output: 'server',
   vite: {
     plugins: [tailwindcss()]
   },
